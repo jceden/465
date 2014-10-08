bb6e426765ccb21f48f50f544b551313d7289bba
jceden <jeden1@mail.csuchico.edu>
2014-10-08 03:15:31 +0000

    Results page configuration, not working but close

--- wladder/app/controllers/home_controller.rb
+++ wladder/app/controllers/home_controller.rb
@@ -7,6 +7,38 @@ class HomeController < ApplicationController
   end
 
   def show
+	@array = Array.new
+	@start = params[:word0]
+	@word1 = params[:guess1]
+	@word2 = params[:guess2]
+	@word3 = params[:guess3]
+	@word4 = params[:guess4]
+	@word5 = params[:guess5]
+	@end = params[:word6]
+	@array.push(@start)
+	if @word1 == /^[a-zA-Z\]*$/
+		@array.push(@word1)
+	end
+	if @word2 == /^[a-zA-Z\]*$/
+		@array.push(@word2)
+	end
+	if @word3 == /^[a-zA-Z\]*$/
+		@array.push(@word3)
+	end
+	if @word4 == /^[a-zA-Z\]*$/
+		@array.push(@word4)
+	end
+	if @word5 == /^[a-zA-Z\]*$/
+		@array.push(@word5)
+	end
+	@array.push(@end)
+	@array.compact!
+	if legal @array
+		@result = "You win!"
+	else
+		@result = "You lose"
+	end
+	@array.compact!
   end
 
 end
