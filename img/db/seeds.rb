a651ae2421a9b45b89db4e22fb7721ced2908b9e
jceden <jeden1@mail.csuchico.edu>
2014-11-16 07:03:53 +0000

    seed file created with....a few things

--- img/db/seeds.rb
+++ img/db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.create! :name => 'Arnold', :email => 'arnold@mail.com', :password => 'asdf', :password_confirmation => 'asdf'
+user2 = User.create! :name => 'Joey Stone', :email => 'j.stone@mail.com', :password => 'fdsa', :password_confirmation => 'fdsa'
+user3 = User.create! :name => 'Guyman Dude', :email => 'guyman@dude.com', :password => '12345', :password_confirmation => '12345'
+
+image1 = Image.create! :filename => 'DVCTZSSANHHVTQZ.jpg', :private => 'false', :user_id => 1
+image2 = Image.create! :filename => 'GTXSZMAFBCUMUUA.jpg', :private => 'false', :user_id => 1
+image3 = Image.create! :filename => 'GYAXKQNKSAQORLW.jpg', :private => 'false', :user_id => 1
+image4 = Image.create! :filename => 'INLHHSCKLLRWJJD.jpg', :private => 'false', :user_id => 1
+image5 = Image.create! :filename => 'JPYWJZKOWPYQSYE.jpg', :private => 'false', :user_id => 2
+image6 = Image.create! :filename => 'POXYZSVLKUFABFR.jpg', :private => 'false', :user_id => 2
+image7 = Image.create! :filename => 'PVBMGUZMXANMOGR.jpg', :private => 'false', :user_id => 2
+image8 = Image.create! :filename => 'RUBKAHEQSOYOZFX.jpg', :private => 'false', :user_id => 3
+image9 = Image.create! :filename => 'TEMFWMKYKJOJVXX.jpg', :private => 'false', :user_id => 3
+
+tag1 = Tag.create! :tag_string => 'image tag', :image_id '2'
+
+access1 = Access.create! :image_id => '1', :user_id => '3'
